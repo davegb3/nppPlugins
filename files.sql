b97682b8a9c2825ba0703792112d9d4eeb8fbb65
kapilratnani <kapil.ratnani@iiitb.net>
2011-03-24 20:29:56 -0700

    Edited files.sql via GitHub

--- files.sql
+++ files.sql
@@ -296,6 +296,6 @@ insert into FileHash(md5sum, filename, pluginName, addedDate, status) values ('6
 insert into FileHash(md5sum, filename, pluginName, addedDate, status) values ('2e318301dbefe51f0de75aaf22953e77','PythonScript.dll','','2011-03-06','ok');
 insert into FileHash(md5sum, filename, pluginName, addedDate, status) values ('a32322665d39998eeef9abaa37ea7217','FingerText.dll','','2011-03-06','ok');
 insert into FileHash(md5sum, filename, pluginName, addedDate, status) values ('7f39133444ffbff0c619a09a0b0c38ce','AnalysePlugin.dll','','2011-03-06','ok');
-insert into FileHash(md5sum, filename, pluginName, addedDate, status) values ('f1e29bd1ffede7452e8a5c336d086001','NPPJSONViewer.dll','','2011-03-06','ok');
 insert into FileHash(md5sum, filename, pluginName, addedDate, status) values ('0cfe3e6182add839d2748a3986b8c5ab','ctags.exe','','2011-03-06','ok');
+insert into FileHash(md5sum, filename, pluginName, addedDate, status) values ('4470434a3d0c9f3e2f4a18418d583c5d','NPPJSONViewer.dll','','2011-25-06','ok');
 commit;
